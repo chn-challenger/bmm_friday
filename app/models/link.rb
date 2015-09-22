e7bc03081a0bb8452cab45fce139e6c3877d9c39
Jongmin Kim <kjm141215@gmail.com>
2015-09-22 09:54:52 +0100

    Added Link class

---
+++ app/models/link.rb
@@ -0,0 +1,10 @@
+# This class corresponds to a table in the database
+# We can use it to manipulate the database
+class Link
+  # adds datamapper functionality to this class
+  include DataMapper::Resource
+  # these property declarations set the column headers in the Link table
+  property :id, Serial # Serial means that it will be auto-incremented for every record
+  property :title, String
+  property :url, String
+end
